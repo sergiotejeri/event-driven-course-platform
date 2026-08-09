4d9f15f7e530635ebc0d142e5ffecb1bb258b245
Sergio Tejerina <sergio.tejeri@gmail.com>
2026-08-09 23:49:27 +0200

    construcción: inicializar proyecto Spring Boot

---
+++ src/main/java/com/acme/courseplatform/CoursePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.acme.courseplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acme/courseplatform/CoursePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.courseplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
